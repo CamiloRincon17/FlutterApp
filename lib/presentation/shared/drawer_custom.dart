956cfdae6bc286749d802587e009ecbbaa453fe5
camilo <camilo171512@gmail.com>
2026-08-31 19:17:07 -0500

    jhon canson

--- lib/presentation/shared/drawer_custom.dart
+++ lib/presentation/shared/drawer_custom.dart
@@ -5,14 +5,57 @@ class DrawerCustom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(child: Column(
-      children: [
-        Expanded(child: ListView()
-        ),
-        ListTile(
-          title: Text("final"),
-        )
-      ],
-    ));
+    return Drawer(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(color: const Color.fromARGB(255, 132, 196, 37)),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundImage: NetworkImage(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0gxjKW7fRq_8snyW0pxT7KlwddGQpI1WlXChdRrs7Pr9Qhfr004nry1g&s=10",
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                "mi name",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40,
+                                ),
+                              ),
+                              Text(
+                                "mi titulo",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(title: Text("final")),
+        ],
+      ),
+    );
   }
 }
